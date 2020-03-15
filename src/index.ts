420421a5af90b17e6dd0f81ede97ae684fc9283f
Junyoung Clare Jang <jjc9310@gmail.com>
2020-03-14 23:30:34 -0400

    Add index files

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as zeroth from './zeroth';
+
+export {
+  zeroth,
+};
---
+++ src/zeroth/index.ts
@@ -0,0 +1,9 @@
+import * as parser from './parser';
+import * as types from './types';
+import * as unification from './unification';
+
+export {
+  parser,
+  types,
+  unification,
+};
